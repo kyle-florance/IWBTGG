b0d5234f905ff29dfb85b8d421eaf7fa7669f406
kyle-florance <kyle.florance@yahoo.com>
2018-12-09 00:16:02 -0700

    updated levels
    HUB now has 6 teleporters for the 6 worlds, also have been connected.
    Doors changed to teleporters, also added a moving spike prefab

---
+++ I WANNA BE THE GG/Assets/Scripts/moveSpike.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveSpike : MonoBehaviour {
+
+    public Transform targetMarker;
+    public float speed;
+    bool triggered = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            triggered = true;
+        }
+    }
+
+    void Update ()
+    {
+        if(triggered && transform.position != targetMarker.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetMarker.position, speed * Time.deltaTime);
+        }
+    }
+}
